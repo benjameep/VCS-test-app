a122b1c4682e0d278c9ead045ece3ef30ec6428b
prich <richluekpakor@yahoo.com>
2017-03-01 09:24:12 -0700

    Created the functions for the property class

--- app/src/main/java/com/benjameep/vcs_test_app/Property.java
+++ app/src/main/java/com/benjameep/vcs_test_app/Property.java
@@ -5,4 +5,24 @@ package com.benjameep.vcs_test_app;
  */
 
 public class Property {
+    private int dev;
+    private int values;
+
+
+    public int getDev() {
+        return dev;
+    }
+
+    public void setDev(int dev) {
+        this.dev = dev;
+    }
+
+    public int getValues() {
+        return values;
+    }
+
+    public void setValues(int values) {
+        this.values = values;
+    }
+
 }
