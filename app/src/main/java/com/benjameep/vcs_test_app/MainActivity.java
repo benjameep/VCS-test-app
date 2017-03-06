23e600349c3103cf790d4664d842a9d6b31843c7
Benjamin Earl <benearl72@gmail.com>
2017-03-06 09:08:25 -0700

    some edits

--- app/src/main/java/com/benjameep/vcs_test_app/MainActivity.java
+++ app/src/main/java/com/benjameep/vcs_test_app/MainActivity.java
@@ -38,16 +38,17 @@ public class MainActivity extends AppCompatActivity {
         public String loadJSONFromAsset() {
             String json = null;
             try {
-                InputStream is = getActivity().getAssets().open("yourfilename.json");
+                InputStream is = getActivity().getAssets().open("numbers.json");
                 int size = is.available();
                 byte[] buffer = new byte[size];
                 is.read(buffer);
                 is.close();
                 json = new String(buffer, "UTF-8");
-            } catch (IOException ex) {
+            } catch (java.io.IOException ex) {
                 ex.printStackTrace();
                 return null;
             }
             return json;
         }
 }
+d
