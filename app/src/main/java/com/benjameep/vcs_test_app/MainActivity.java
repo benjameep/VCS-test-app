57c08d75dd75038cc6ef51ce65fc23cb2e823584
benjameep <benearl72@gmail.com>
2017-03-31 10:51:04 -0600

    Added what i think is what drag and drop look like

--- app/src/main/java/com/benjameep/vcs_test_app/MainActivity.java
+++ app/src/main/java/com/benjameep/vcs_test_app/MainActivity.java
@@ -1,9 +1,15 @@
 package com.benjameep.vcs_test_app;
 
+import android.annotation.SuppressLint;
 import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
@@ -18,6 +24,9 @@ public class MainActivity extends AppCompatActivity {
         public static final String NUM_PLAYERS_KEYWORD = "numPlayers";
         private Game game;
         private Data[] _jsonData;
+        private int xDelta;
+        private int yDelta;
+        private ViewGroup mainLayout;
 
     private static final String TAG = MainActivity.class.getSimpleName();
 
@@ -53,6 +62,9 @@ public class MainActivity extends AppCompatActivity {
             Log.d("Game",gson.toJson(this.game));
             this.game.updatePlayersBalance();
 
+            // DragNDrop
+            mainLayout = (RelativeLayout) findViewById(R.id.activity_main);
+            findViewById(R.id.prop_0).setOnTouchListener(onTouchListener());
         }
 
         @Override
@@ -85,5 +97,47 @@ public class MainActivity extends AppCompatActivity {
                 return null;
             }
             return json;
+        }
+
+        private View.OnTouchListener onTouchListener() {
+            return new View.OnTouchListener() {
+
+                @SuppressLint("ClickableViewAccessibility")
+                @Override
+                public boolean onTouch(View view, MotionEvent event) {
+
+                    final int x = (int) event.getRawX();
+                    final int y = (int) event.getRawY();
+
+                    switch (event.getAction() & MotionEvent.ACTION_MASK) {
+
+                        case MotionEvent.ACTION_DOWN:
+                            RelativeLayout.LayoutParams lParams = (RelativeLayout.LayoutParams)
+                                    view.getLayoutParams();
+
+                            xDelta = x - lParams.leftMargin;
+                            yDelta = y - lParams.topMargin;
+                            break;
+
+                        case MotionEvent.ACTION_UP:
+                            Toast.makeText(MainActivity.this,
+                                    "thanks for new location!", Toast.LENGTH_SHORT)
+                                    .show();
+                            break;
+
+                        case MotionEvent.ACTION_MOVE:
+                            RelativeLayout.LayoutParams layoutParams = (RelativeLayout.LayoutParams) view
+                                    .getLayoutParams();
+                            layoutParams.leftMargin = x - xDelta;
+                            layoutParams.topMargin = y - yDelta;
+                            layoutParams.rightMargin = 0;
+                            layoutParams.bottomMargin = 0;
+                            view.setLayoutParams(layoutParams);
+                            break;
+                    }
+                    mainLayout.invalidate();
+                    return true;
+                }
+            };
         }
 }
